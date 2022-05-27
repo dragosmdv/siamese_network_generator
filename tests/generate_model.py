def6c927698902d50ccfec6bb81dfb2c2dc9df33
Dragos Moldovan <moldovan.dragos13@yahoo.ro>
2022-05-27 12:36:24 +0300

    Update lib and test

--- tests/generate_model.py
+++ tests/generate_model.py
@@ -6,14 +6,12 @@ if __name__ == "__main__":
     save_path = "generated_models/myModel_v16"
     epochs = 1
     generate_model(test_file, train_file, save_path, epochs)
-    '''distance should be > 0  and the resulted tensor should be ('Opera_Națională_Română_din_Cluj-Napoca', 
-    <tf.Tensor: shape=(1,), dtype=float32, numpy=array([1.150906], dtype=float32)>) 
+    '''
+    distance should be > 0  and the resulted tensor should be ('Opera_Națională_Română_din_Cluj-Napoca') 
     '''
     print(test_prediction("test_image.jpg", save_path, test_file))
 
-    '''given a sample from the train dataset, distance should be =0  and the resulted tensor should be (
-    'Biserica_evanghelică_din_Cluj-Napoca', <tf.Tensor: shape=(1,), dtype=float32, numpy=array([0.], dtype=float32)>) 
     '''
-    print(test_prediction(
-        "datasets/test_images/Biserica_evanghelică_din_Cluj-Napoca/Biserica evanghelică din Cluj-Napoca_18.jpeg",
-        save_path, test_file))
+    given a sample from the train dataset, distance should be = 0 and the resulted tensor should be ('Biserica_evanghelică_din_Cluj-Napoca') 
+    '''
+    print(test_prediction("datasets/test_images/Biserica_evanghelică_din_Cluj-Napoca/Biserica evanghelică din Cluj-Napoca_18.jpeg", save_path, test_file))
